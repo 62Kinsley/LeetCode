c8e04399f9425312c8bc3254bc0800d9d82b1ca2
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2025-12-01 23:30:31 -0800

    LC-maximum-subarray-sum-with-length-divisible-by-k [Runtime: 4 ms; Memory: 149.2 MB]

--- hash-table/maximum-subarray-sum-with-length-divisible-by-k.java
+++ hash-table/maximum-subarray-sum-with-length-divisible-by-k.java
@@ -1,34 +1,52 @@
+// class Solution {
+//     public long maxSubarraySum(int[] nums, int k) {
+        
+//         //1.prefix sum
+//         //[-5, 1, 2, -3,  4]
+// //prefix   0  -5,-4, -2, -5, -1
+// // /只有当两个前缀和的索引对 k 取余相同时，它们的差才代表一个长度被 k 整除的子数组。
+// //r-l+1 % k == 0?  -> r+1 % k == l%k
+// //4+1 % 4 == 1%4
+//         int n = nums.length;
+//         long[] prefix = new long[n+1];
+    
+//         for(int i=1; i<=n; i++){
+//             prefix[i] = prefix[i-1] + nums[i-1];
+        
+//         }
+
+//         long ans = Long.MIN_VALUE;
+//         long[] minPrefix = new long[k];//因为余数最多不会超过k
+//         Arrays.fill(minPrefix, Long.MAX_VALUE/2);
+
+//         for(int j=0; j<=n; j++){
+//             int mod = j % k;//1%4=1, 5%4=1, 9%4=1
+            
+//             ans = Math.max(prefix[j] - minPrefix[mod], ans);//必须要先计算，后更新，因为相减要保证长度为kork的倍数
+//             //如果先更新了，万一这个min的是自己，后面求ans的时候，再自己减自己，就不够了
+//             minPrefix[mod] =  Math.min(minPrefix[mod], prefix[j]);
+//         }
+
+//         return ans;
+      
+//     }
+// }
+
+
 class Solution {
     public long maxSubarraySum(int[] nums, int k) {
-        
-        //1.prefix sum
-        //[-5, 1, 2, -3,  4]
-//prefix   0  -5,-4, -2, -5, -1
-// /只有当两个前缀和的索引对 k 取余相同时，它们的差才代表一个长度被 k 整除的子数组。
-//r-l+1 % k == 0?  -> r+1 % k == l%k
-//4+1 % 4 == 1%4
-        int n = nums.length;
-        long[] prefix = new long[n+1];
-    
-        for(int i=1; i<=n; i++){
-            prefix[i] = prefix[i-1] + nums[i-1];
-        
-        }
+        long[] minS = new long[k];
+        Arrays.fill(minS, 0, k - 1, Long.MAX_VALUE / 2); // 防止下面减法溢出
 
         long ans = Long.MIN_VALUE;
-        long[] minPrefix = new long[k];//因为余数最多不会超过k
-        Arrays.fill(minPrefix, Long.MAX_VALUE/2);
-
-        for(int j=0; j<=n; j++){
-            int mod = j % k;//1%4=1, 5%4=1, 9%4=1
-            
-            ans = Math.max(prefix[j] - minPrefix[mod], ans);//必须要先计算，后更新，因为相减要保证长度为kork的倍数
-            //如果先更新了，万一这个min的是自己，后面求ans的时候，再自己减自己，就不够了
-            minPrefix[mod] =  Math.min(minPrefix[mod], prefix[j]);
+        long s = 0;
+        for (int j = 0; j < nums.length; j++) {
+            s += nums[j];
+            int i = j % k;
+            ans = Math.max(ans, s - minS[i]);
+            minS[i] = Math.min(minS[i], s);
         }
-
         return ans;
-      
     }
 }
 
