60278d1d0c678de296868e4bc8c15cc1b966f89f
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2025-12-03 21:51:38 -0800

    LC-array-partition [Runtime: 5 ms; Memory: 47.8 MB]

--- counting-sort/array-partition.java
+++ counting-sort/array-partition.java
@@ -1,20 +1,36 @@
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         int res = 0;
+//         for(int i=0; i<n; i+=2){
+            
+//             res += nums[i];
+//         }
+
+//         return res;
+//     }
+// }
+
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int res = 0;
-        for(int i=0; i<n; i+=2){
-            
-            res += nums[i];
-            
-           
-        }
+        int[] bucket = new int[20001];
 
+        for(int num : nums){
+            bucket[num+10000]++;
+        }
+        int res = 0;
+        boolean add = true;
+        for(int i=0; i<=20000; i++){
+            while(bucket[i] > 0){
+                if(add){
+                    res +=(i- 10000);//1+2
+                }
+                add = !add;//t
+                bucket[i]--;//
+            }
+        }
         return res;
+
     }
 }
-
-// 1,2, 3,4
-// 6,2, 6,5, 1,2
-
-// [1,2, 2,5, 6,6]:
