c0d709d690b633c20ae3ce796b1583b0ace893fa
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2025-12-03 18:01:21 -0800

    LC-lexicographically-smallest-negated-permutation-that-sums-to-target [Runtime: N/A; Memory: N/A]

---
+++ math/lexicographically-smallest-negated-permutation-that-sums-to-target.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] lexSmallestNegatedPerm(int n, long target) {
+        //edge case：
+    
+        int sum  = (n+1)*n/2;      
+        if(sum < target){
+            return new int[0];
+        }
+        if(-sum > target){
+            return new int[0];
+        }
+        if((sum - target)%2 == 1){
+            return new int[0];
+        }
+      
+
+        long d = (sum - target);
+        int[] res = new int[n];
+        int l=0, r=n-1;
+       
+        for(int x=n; x>=1; x--){
+            if(d >= 2*x){
+                d -= x;
+                res[l] = -x;
+                l++;
+            }else{
+                d -= x;
+                res[r] = x;
+                r--;
+            }
+        }
+
+        return res;
+
+    }
+}
