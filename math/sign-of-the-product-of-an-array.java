00bacff6293a8e637222d32eecfa21bcfdb9849b
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2025-12-18 10:54:57 -0800

    LC-sign-of-the-product-of-an-array [Runtime: 0 ms; Memory: 45.4 MB]

--- math/sign-of-the-product-of-an-array.java
+++ math/sign-of-the-product-of-an-array.java
@@ -1,19 +1,41 @@
-class Solution {
-    public int arraySign(int[] nums) {
-        int product = 1;
-        for(int num : nums){
-            if(num == 0){
-                return 0;
-            }
-            if(num < 0){
-                product *= -1;
-            }
-             if(num > 0){
-                product *= 1;
-            }  
-        }
-        return product;
+// class Solution {
+//     public int arraySign(int[] nums) {
+//         int product = 1;
+//         for(int num : nums){
+//             if(num == 0){
+//                 return 0;
+//             }
+//             if(num < 0){
+//                 product *= -1;
+//             }
+//              if(num > 0){
+//                 product *= 1;
+//             }  
+//         }
+//         return product;
       
 
+//     }
+// }
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int negCount = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                return 0;
+            }
+            if(nums[i]<0){
+                negCount++;
+            }
+        }
+        if(negCount%2==0){
+            return 1;
+        }else{
+            return -1;
+        }
+
+        
     }
+
 }
