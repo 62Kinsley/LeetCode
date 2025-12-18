1e7f2eddacfe77104616ee7dc4c75ef7505b75fa
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2025-12-18 14:52:15 -0800

    LC-maximum-running-time-of-n-computers [Runtime: N/A; Memory: N/A]

--- binary-search/maximum-running-time-of-n-computers.java
+++ binary-search/maximum-running-time-of-n-computers.java
@@ -1,38 +1,35 @@
-// class Solution {
-//     public long maxRunTime(int n, int[] batteries) {
-        
-//     }
-
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
-        long sum = 0;
-        for (int b : batteries) {
-            sum += b;
+
+        int sum = 0;
+        for(int x : batteries){
+            sum += x;
         }
+
+        int l=0, r = sum/n;
+        while(l < r){
+            int mid = l + (r-l) / 2;
+            int estimatedTotal = check(batteries, mid);
+            if(mid*n >  estimatedTotal ){//预期的target太大了
+                    r = mid;
+            }else{//target*n <=  estimatedTotal, 
+                l = mid+1;
+            }
+        }
+        return l;
         
-        // 二分查找的范围
-        long left = 0, right = sum / n;
+    }
 
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            
-            // 所有电池能提供的最大有效电量
-            long valid_power = 0;
-            for (int b : batteries) {
-                valid_power += Math.min(b, mid);
+
+    private int check(int[] batteries, int target){
+            int total = 0;
+
+            for(int battery : batteries){
+                total += Math.min(battery, target);//
             }
-
-            if (valid_power >= (long) n * mid) {
-                // mid 可行，尝试更大的时间
-                left = mid + 1;
-            } else {
-                // mid 太大，需要减小目标
-                right = mid - 1;
-            }
-        }
-
-        // 循环结束时，left 指向 right + 1
-        return right;
+            return total;
     }
 }
+
+
 
