27e6a3e6085689132c432eb2b5313dbd0a6073e8
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2025-12-17 23:38:01 -0800

    LC-rearrange-array-elements-by-sign [Runtime: 3 ms; Memory: 176 MB]

--- two-pointers/rearrange-array-elements-by-sign.java
+++ two-pointers/rearrange-array-elements-by-sign.java
@@ -1,34 +1,24 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        int l = 0;
-        int r = nums.length -1;
+        int pos = 0;
+        int neg = 1;
         int[] res = new int[n];
-        int i=0, j=nums.length -1;  
-        
-    //                l             
-    //     [3,1,-2,-5,2,-4]
-    //        r
 
-    //                i
-    //     [3,-2,1,-5, 2 -4]
-    //   j
-        while(i < n-1 || j>0){
-            if(nums[l] > 0){
-                res[i] = nums[l];
-                i+=2;
+        for(int x : nums){
+            if(x > 0){
+                res[pos] = x;
+                pos += 2;
+            }else{
+                res[neg] = x;
+                neg += 2;
             }
-            l++;
-            
-
-            if(nums[r] < 0){
-                res[j] = nums[r];
-                j-=2;
-            }
-            r--;
-            
         }
+      
         return res;
+
+        
+
        
     }
 }
