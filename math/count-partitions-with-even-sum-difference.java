40086b3335bf74623b92f37a2197584b893e87d6
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2025-12-18 11:24:08 -0800

    LC-count-partitions-with-even-sum-difference [Runtime: 0 ms; Memory: 44 MB]

---
+++ math/count-partitions-with-even-sum-difference.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+        int res = 0;
+
+        for(int i=0; i<n; i++){
+            right += nums[i];
+        }
+
+        for(int i=0; i<n-1; i++){
+            left += nums[i];
+            right -= nums[i];
+
+            if((left - right) %2 == 0){
+                res++;
+            }
+        }
+
+        return res;
+
+        
+    }
+}
