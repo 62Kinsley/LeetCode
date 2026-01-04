73338fedc8b5fa1bff2427dc6bea0f199cdc3766
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2026-01-04 01:48:25 -0800

    LC-minimum-penalty-for-a-shop [Runtime: 28 ms; Memory: 77.7 MB]

---
+++ string/minimum-penalty-for-a-shop.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        //
+        int times = customers.length();
+    
+        // 4: 0+0+1+0
+        //    Y Y N Y
+        // 3: 0+0+1+1
+        // 2: 0+0+0 +1
+        // 1: 0+
+
+        List<Integer>[] penalty = new ArrayList[times+1];//penalty:<time>
+        int count = 0;
+        for(char c :customers.toCharArray() ){
+            if(c == 'N'){
+                count++;
+            }
+        }
+        penalty[count] = new ArrayList<>();
+        penalty[count].add(times);//4:<1>
+
+        for(int i=times-1; i>=0; i--){//现在是关门状态
+            char c = customers.charAt(i);
+            if(c == 'Y'){//如果有人来，那罚款就比刚刚开门的时候多
+                count++;
+            }
+            if (c == 'N'){//如果没有人来，因为刚刚是开门，没人来就要扣款， 现在是关门，没人来就没关系，所以不用扣款，把刚刚的扣款减掉
+                count--;
+            }
+
+            if(penalty[count] == null){
+                penalty[count] = new ArrayList<>();
+            }
+            penalty[count].add(i);
+        }
+        
+        int res = times;
+        for(int i=0; i<=times; i++){
+            if(penalty[i] != null){
+                List<Integer> time = penalty[i];
+                for(int t : time){
+                    if(t < res){
+                        res = t;
+                    }
+                }
+                break;
+            }
+            
+        }
+        return res;
+    }
+}
