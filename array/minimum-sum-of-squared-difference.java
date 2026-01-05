8473f9da036875954b4c0b58681c0be2bbed65eb
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2026-01-04 22:48:47 -0800

    LC-minimum-sum-of-squared-difference [Runtime: 6 ms; Memory: 110.6 MB]

--- array/minimum-sum-of-squared-difference.java
+++ array/minimum-sum-of-squared-difference.java
@@ -5,31 +5,36 @@ class Solution {
         int[] diffTimes = new int[100001];//0-10000, index:diff value:times
         int maxDiff = 0;
         long k = k1+k2;
+        long totalDiff = 0;
 
         for(int i=0; i<n; i++){
             int curDiff = Math.abs(nums1[i] - nums2[i]);
+            totalDiff += curDiff;//提前算
             diffTimes[curDiff]++;
-            if(curDiff > maxDiff){
-                maxDiff = curDiff;
-            }
+            maxDiff = Math.max(maxDiff, curDiff);
         }
-
+        if (totalDiff <= k) {
+            return 0;
+        }
         for(int i=maxDiff; i>0&& k>0; i--){
-            if(k >= diffTimes[i]){
-                k -= diffTimes[i];
-                diffTimes[i-1] += diffTimes[i];
-                diffTimes[i] = 0;
-            }else{//k < diffTimes[i]
-                diffTimes[i-1] += k;
-                diffTimes[i] -= k;
-                k = 0;
+            if(diffTimes[i] > 0){
+                if(k >= diffTimes[i]){
+                    k -= diffTimes[i];
+                    diffTimes[i-1] += diffTimes[i];
+                    diffTimes[i] = 0;
+                }else{//k < diffTimes[i]
+                    diffTimes[i-1] += k;
+                    diffTimes[i] -= k;
+                    k = 0;
+                }
             }
+            
         }
 
         long res = 0;
         for(int i=maxDiff; i>0; i--){
             if(diffTimes[i] > 0){
-                res += diffTimes[i]* i * i;
+                res += (long) diffTimes[i]* i * i;
             }  
         }
         return res;
