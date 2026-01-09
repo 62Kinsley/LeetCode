fe1bd8fb4fdee693eae1cdfa310a055d4815bca9
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2026-01-08 18:23:00 -0800

    LC-palindrome-linked-list [Runtime: 6 ms; Memory: 94.4 MB]

--- stack/palindrome-linked-list.java
+++ stack/palindrome-linked-list.java
@@ -8,28 +8,59 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-
-//     //1.find the midlle node
-//     public boolean isPalindrome(ListNode head) {
-        
-//     }
-
-//     public
-// }
-
 class Solution {
+
+    //1.find the middle node (odd even)
+    // 2.create new linked list which would be reversed of the original linked list.
+    //3.  just need the second half of linked list to be reversed.
+    //4. After getting middle node, reverse the list after that node.
+
+    //1->2->2->1         //1->2->3->2->1
+
+    //1->2  2<-1        //1->2->3 <-2<-1
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack = new Stack();
-        ListNode curr = head;
-        while(curr != null) {
-            stack.push(curr.val);
-            curr = curr.next;
+
+        ListNode middle = middleNode(head);//2 3
+        ListNode head2 = reverse(middle);
+
+        while(head2 != null){
+             if(head2.val != head.val){
+                return false;
+            }
+            head2 = head2.next;
+            head = head.next;
         }
-        curr = head;
-        while(curr != null && curr.val == stack.pop()) {
-            curr = curr.next;
+        return true;
+    }
+
+    public ListNode middleNode(ListNode head){//two pointer, fast, slow
+        ListNode fast = head;
+        ListNode slow = head;
+             
+         //1->2->3->2->1
+                
+        while(fast != null && fast.next != null){
+            slow = slow.next;// speed of 1
+            fast = fast.next.next;// speed of 2
         }
-        return curr == null;
+        return slow;
+
+    }
+
+    //reverse the linkedlist
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode cur = head;
+            
+        //<-3 <-2<-1
+        while(cur != null){
+              ListNode next = cur.next;//null
+              cur.next = prev;
+              prev = cur;
+              cur = next;
+        }
+        return prev;
+
     }
 }
+
