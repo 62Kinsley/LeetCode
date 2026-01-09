d936d1f27f99c4d5a6b61b7c22fa1849712b77e4
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2026-01-08 19:53:18 -0800

    LC-set-mismatch [Runtime: 2 ms; Memory: 47.8 MB]

--- array/set-mismatch.java
+++ array/set-mismatch.java
@@ -1,12 +1,21 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        
+        //[1,2,3,4,2,6]
         int[] res = new int[2];
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]!= i+1){
-                res[0] = nums[i];
-                res[1] = i+1;
-                break;
+        int n = nums.length;
+        int[] record = new int[n+1];
+        //[0,1,1,0,1]
+        //[1,1]
+        for(int i=0; i<n; i++){
+            int num = nums[i];
+            if(record[num] == 1){
+                res[0] = num;
+            }
+            record[num]++;
+        }
+        for(int i=1; i<n+1; i++){
+            if(record[i] == 0){
+                res[1] = i;
             }
         }
         return res;
