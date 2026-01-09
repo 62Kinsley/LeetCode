bdcd7dadf5dbab1ea7a656aed990d845cc32db3f
Kinsley <144962560+62Kinsley@users.noreply.github.com>
2026-01-08 20:51:32 -0800

    LC-maximum-matrix-sum [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximum-matrix-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        //1.
+        int n = matrix.length;
+        int countNeg = 0;
+        int res = 0;
+        int minAbs = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] <= 0){
+                    countNeg++;
+                    minAbs = Math.min(minAbs, -matrix[i][j]);
+                }
+                res += Math.abs(matrix[i][j]);//所有数的绝对值都相加
+            }
+        }
+
+        //偶数的时候就直接输出答案
+        //奇数的话，就看有没有0，其实不管有没有0，都不用特地的去判断
+        //因为刚刚相加所有的数的绝对值的时候，
+        //如果有0 那么得到的结果就是(0+正数+abs(负数)), 这时候 minAbs是0，所以res-2* minAbs = res
+        //如果没有0， 那么得到的结果就是 正数+abs(负数) 这时候 minAbs是abs（最大的负数）所以 最终的结果就是res要➖2倍的这个绝对值，
+        if(countNeg % 2 != 0){
+            res -= 2* minAbs;
+        }
+        return res;
+    }
+}
