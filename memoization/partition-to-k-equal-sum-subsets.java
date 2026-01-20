63a03f823c8cf85f30d4b2baff3a56ff1c35e0a1
QijianSu <su.qin@northeastern.edu>
2026-01-19 22:00:33 -0800

    LC-partition-to-k-equal-sum-subsets [Runtime: 9 ms; Memory: 43.4 MB]

--- memoization/partition-to-k-equal-sum-subsets.java
+++ memoization/partition-to-k-equal-sum-subsets.java
@@ -1,75 +1,65 @@
-// class Solution {
-//        //1.calculate the sum of nums
-//         //2.calculate the sum of every subsets
-//         //if sum of every subsets is != Integer (sum % k != 0)
-//         //such as: example2 10/3 != integer
-//         //return false;
-//         //sort nums: [1,2,2,3,3,4,5]
-//     public boolean canPartitionKSubsets(int[] nums, int k) {
-        
-//         int sum = 0;//12
-//         for(int num : nums){
-//             sum += num;
-//         }
-
-//         if(sum % k != 0){
-//             return false;
-//         }
-
-//         int need = sum / k; 
-//         int n = nums.length;
-//         boolean[] visited = new boolean[n];
-//         Arrays.sort(nums);
-//         return backTracking(nums, need, 0, 0);
-        
-
-//     }
-
-//     public boolean backTracking(int[] nums, int target, int sum, int count, boolean[] visited){
-        
-//          //termination condition: handle all element
-//          if(index == nums.length){
-
-//          }
-
-//          for(int i=0; i<)
-
-
-
-//     }
-// }
-
-
-
 class Solution {
+       //1.calculate the sum of nums
+        //2.calculate the sum of every subsets
+        //if sum of every subsets is != Integer (sum % k != 0)
+        //such as: example2 10/3 != integer
+        //return false;
+        //sort nums: [1,2,2,3,3,4,5]
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        int sum=0;
-        for(int a:nums){
-            sum+=a;
+        
+        int sum = 0;//12
+        for(int num : nums){
+            sum += num;
         }
-        if(sum%k!=0) return false;
-        int a=sum/k;
-        // Arrays.sort(nums);
-        boolean[] b=new boolean[nums.length];
-        return isTrue(nums,k,a,0,b,0);
+
+        if(sum % k != 0){
+            return false;
+        }
+
+        int need = sum / k; 
+        int n = nums.length;
+        boolean[] visited = new boolean[n];
+        Arrays.sort(nums);
+        return backTracking(nums, 0, visited, 0, need, 0, k);
+        
+
     }
-    public boolean isTrue(int[] a,int k,int tar,int i,boolean[] b,int curr){
-        if(k==1){
+
+    public boolean backTracking(int[] nums, int startIndex, boolean[] visited, int curSum, int target, int count, int k ){
+        
+        //termination condition: handle all element
+        if(count == k){
             return true;
         }
-        if(curr==tar){
-            return isTrue(a,k-1,tar,0,b,0);
+        if(curSum == target){
+            count++;//4
+            return backTracking(nums, 0, visited, 0, target, count, k);
         }
-        for(int j=i;j<a.length;j++){
-            if(b[j]) continue;
-            if(curr+a[j]>tar) continue;
-            b[j]=true;
-            if(isTrue(a,k,tar,j+1,b,curr+a[j])) return true;
-            b[j]=false;
-            if(curr==0){
-                break;
+        //  0 1 2 3 4 5 6
+        // [4,3,2,3,5,2,1]
+        // [t f f f f f t]
+
+        for(int i=startIndex; i<nums.length; i++){//1
+            if(visited[i]){
+                continue;
+            }
+            if(curSum + nums[i] > target){//6
+                continue;
+            }
+            visited[i] = true;
+            if(backTracking(nums, i+1, visited, curSum+nums[i], target, count, k)){//1, 4,5, 0,4
+                return true;
+            }
+            visited[i] = false;//回溯
+            while(i+1< nums.length && nums[i+1] == nums[i]){
+                i++;
             }
         }
         return false;
+
+
     }
 }
+
+
+
