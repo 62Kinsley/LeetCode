1a17aeeb349db48d5dfdcf92655971aca4bdc928
QijianSu <su.qin@northeastern.edu>
2026-01-29 21:38:16 -0800

    LC-design-hit-counter [Runtime: 1 ms; Memory: 43 MB]

--- array/design-hit-counter.java
+++ array/design-hit-counter.java
@@ -1,58 +1,26 @@
+
  class HitCounter {
-    private int[] times;
-    private int[] hits;
-    /** Initialize your data structure here. */
-    public HitCounter() {
-        times = new int[300];
-        hits = new int[300];
-    }
-    
-    /** Record a hit.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    public void hit(int timestamp) {
-        int index = timestamp % 300;
-        if (times[index] != timestamp) {
-            times[index] = timestamp;
-            hits[index] = 1;
-        } else {
-            hits[index]++;
+        Queue<Integer> q;
+        /** Initialize your data structure here. */
+        public HitCounter() {
+            q = new LinkedList<>();
         }
-    }
-    
-    /** Return the number of hits in the past 5 minutes.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    public int getHits(int timestamp) {
-        int total = 0;
-        for (int i = 0; i < 300; i++) {
-            if (timestamp - times[i] < 300) {
-                total += hits[i];
+        
+        /** Record a hit.
+            @param timestamp - The current timestamp (in seconds granularity). */
+        public void hit(int timestamp) {
+            q.offer(timestamp);
+        }
+        
+        /** Return the number of hits in the past 5 minutes.
+            @param timestamp - The current timestamp (in seconds granularity). */
+        public int getHits(int timestamp) {
+            while(!q.isEmpty() && timestamp - q.peek() >= 300) {
+                q.poll();
             }
+            return q.size();
         }
-        return total;
     }
-}
-//  class HitCounter {
-//         Queue<Integer> q = null;
-//         /** Initialize your data structure here. */
-//         public HitCounter() {
-//             q = new LinkedList<Integer>();
-//         }
-        
-//         /** Record a hit.
-//             @param timestamp - The current timestamp (in seconds granularity). */
-//         public void hit(int timestamp) {
-//             q.offer(timestamp);
-//         }
-        
-//         /** Return the number of hits in the past 5 minutes.
-//             @param timestamp - The current timestamp (in seconds granularity). */
-//         public int getHits(int timestamp) {
-//             while(!q.isEmpty() && timestamp - q.peek() >= 300) {
-//                 q.poll();
-//             }
-//             return q.size();
-//         }
-//     }
 /**
  * Your HitCounter object will be instantiated and called as such:
  * HitCounter obj = new HitCounter();
