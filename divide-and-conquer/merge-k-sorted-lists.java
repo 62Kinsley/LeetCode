f202346f8bce4620e91d9943c1da2c874394b6c6
QijianSu <su.qin@northeastern.edu>
2026-01-30 17:00:45 -0800

    LC-merge-k-sorted-lists [Runtime: 4 ms; Memory: 47 MB]

--- divide-and-conquer/merge-k-sorted-lists.java
+++ divide-and-conquer/merge-k-sorted-lists.java
@@ -46,8 +46,6 @@
 //     }
 // }
 
-
-
 // class Solution{
 //    public ListNode mergeKLists(ListNode[] lists) {
 //         if (lists == null || lists.length == 0) return null;
@@ -90,41 +88,67 @@
 // }
 
 
+// class Solution{
+//    public ListNode mergeKLists(ListNode[] lists) {
+//         if (lists == null || lists.length == 0) return null;
+//         return  divide(lists, 0, lists.length - 1);
+//     }
+
+//     private ListNode divide(ListNode[] lists, int left, int right) {
+//         if (left == right) return lists[left];  // [[1,4,5],    [1,3,4],        [2,6]]
+//         int mid = left + (right - left) / 2;
+//         ListNode l1 =  divide(lists, left, mid);
+//         ListNode l2 =  divide(lists, mid + 1, right);
+//         return mergeTwoLists(l1, l2);
+//     }
+
+//     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//         ListNode dummy = new ListNode(0);
+//         ListNode cur = dummy;
+
+//         while (l1 != null && l2 != null) {
+//             if(l1.val < l2.val){
+//                 cur.next = l1;
+//                 l1 = l1.next;
+//             }else{
+//                 cur.next = l2;
+//                 l2 = l2.next;
+//             }
+//             cur = cur.next;
+//         }
+//         if(l1 != null) cur.next = l1;
+//         if(l2 != null) cur.next = l2;
+
+//         return dummy.next;
+//     }
+// }
+
+
 class Solution{
-   public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-        return  divide(lists, 0, lists.length - 1);
-    }
-
-    private ListNode divide(ListNode[] lists, int left, int right) {
-        if (left == right) return lists[left];  // [[1,4,5],    [1,3,4],        [2,6]]
-        int mid = left + (right - left) / 2;
-        ListNode l1 =  divide(lists, left, mid);
-        ListNode l2 =  divide(lists, mid + 1, right);
-        return mergeTwoLists(l1, l2);
-    }
-
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
-
-        while (l1 != null && l2 != null) {
-            if(l1.val < l2.val){
-                cur.next = l1;
-                l1 = l1.next;
-            }else{
-                cur.next = l2;
-                l2 = l2.next;
+    public ListNode mergeKLists(ListNode[] lists){
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b) -> a.val - b.val);
+        
+        for(ListNode node : lists){
+            if(node != null){
+                minHeap.offer(node); //[1,4,5],[1,3,4],[2,6]]
             }
-            cur = cur.next;
+            
         }
-        if(l1 != null) cur.next = l1;
-        if(l2 != null) cur.next = l2;
+
+        ListNode dummy = new ListNode();
+        ListNode  cur = dummy;
+        while(!minHeap.isEmpty()){
+            ListNode node  = minHeap.poll();
+            cur.next = node;
+            cur = cur.next;
+            
+            if (node.next!= null) {
+                minHeap.add(node.next);
+            }   
+            
+        }
 
         return dummy.next;
+
     }
 }
-
-
-
-
