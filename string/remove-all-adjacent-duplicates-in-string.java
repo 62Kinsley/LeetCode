954d856b12a9c402d41b7f396e84b4a9584688df
QijianSu <su.qin@northeastern.edu>
2026-02-02 13:54:43 -0800

    LC-remove-all-adjacent-duplicates-in-string [Runtime: 12 ms; Memory: 47.3 MB]

--- string/remove-all-adjacent-duplicates-in-string.java
+++ string/remove-all-adjacent-duplicates-in-string.java
@@ -1,23 +1,41 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         StringBuilder sb=new StringBuilder(); 
+       
+//         for(char c : s.toCharArray()){
+//             if(!stack.isEmpty()){
+//                 if(c == stack.peek()){
+//                     stack.pop();
+//                     continue;
+//                 }
+//             }
+//             stack.push(c);
+//         }
+
+//         while(!stack.isEmpty()){
+//             sb.append(stack.pop());
+//         }
+
+//         return sb.reverse().toString();
+//     }
+// }
+
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character> stack = new Stack<>();
-        StringBuilder sb=new StringBuilder(); 
+        
+        StringBuilder sb = new StringBuilder(); 
        
         for(char c : s.toCharArray()){
-            if(!stack.isEmpty()){
-                if(c == stack.peek()){
-                    stack.pop();
-                    continue;
-                }
+            int len = sb.length();
+            if(len > 0 && c == sb.charAt(len - 1)){
+                sb.deleteCharAt(len - 1);
+                continue;
             }
-            stack.push(c);
+            sb.append(c);//
         }
 
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
 
