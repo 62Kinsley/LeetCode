bde8b4e773c9cc8149459c52effb73c1aeb00a4f
QijianSu <su.qin@northeastern.edu>
2026-02-02 13:55:54 -0800

    LC-remove-all-adjacent-duplicates-in-string [Runtime: 35 ms; Memory: 47.4 MB]

--- stack/remove-all-adjacent-duplicates-in-string.java
+++ stack/remove-all-adjacent-duplicates-in-string.java
@@ -1,43 +1,43 @@
-// class Solution {
-//     public String removeDuplicates(String s) {
-//         Stack<Character> stack = new Stack<>();
-//         StringBuilder sb=new StringBuilder(); 
-       
-//         for(char c : s.toCharArray()){
-//             if(!stack.isEmpty()){
-//                 if(c == stack.peek()){
-//                     stack.pop();
-//                     continue;
-//                 }
-//             }
-//             stack.push(c);
-//         }
-
-//         while(!stack.isEmpty()){
-//             sb.append(stack.pop());
-//         }
-
-//         return sb.reverse().toString();
-//     }
-// }
-
 class Solution {
     public String removeDuplicates(String s) {
-        
-        StringBuilder sb = new StringBuilder(); 
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb=new StringBuilder(); 
        
         for(char c : s.toCharArray()){
-            int len = sb.length();
-            if(len > 0 && c == sb.charAt(len - 1)){
-                sb.deleteCharAt(len - 1);
-                
-            }else{
-                sb.append(c);
+            if(!stack.isEmpty()){
+                if(c == stack.peek()){
+                    stack.pop();
+                    continue;
+                }
             }
-            //
+            stack.push(c);
         }
 
-        return sb.toString();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
     }
 }
+
+// class Solution {
+//     public String removeDuplicates(String s) {
+        
+//         StringBuilder sb = new StringBuilder(); 
+       
+//         for(char c : s.toCharArray()){
+//             int len = sb.length();
+//             if(len > 0 && c == sb.charAt(len - 1)){
+//                 sb.deleteCharAt(len - 1);
+                
+//             }else{
+//                 sb.append(c);
+//             }
+//             //
+//         }
+
+//         return sb.toString();
+//     }
+// }
 
