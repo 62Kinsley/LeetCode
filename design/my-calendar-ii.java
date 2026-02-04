a751dd3996377027bb5c72cc7c57a54b74717259
QijianSu <su.qin@northeastern.edu>
2026-02-04 13:47:11 -0800

    LC-my-calendar-ii [Runtime: 46 ms; Memory: 47.3 MB]

--- design/my-calendar-ii.java
+++ design/my-calendar-ii.java
@@ -1,13 +1,37 @@
-// class MyCalendarTwo {
+class MyCalendarTwo {
 
-//     public MyCalendarTwo() {
-        
-//     }
+    private List<int[]> bookings;// 所有已预订的事件
+    private List<int[]> overlapBookings;// 所有「已经双重预订」的时间段
+    //如果新事件与「已经双重预订的区间」重叠，那就变成三重预订了！
+    public MyCalendarTwo() {
+        bookings = new ArrayList<>();//(10, 20) (50, 60)
+        overlapBookings = new ArrayList<>();
+    }
+    //// 判断两个区间是否重叠
+    private boolean doesOverlap(int startTime1, int endTime1, int startTime2, int endTime2){
+        return Math.max(startTime1, startTime2) < Math.min(endTime1, endTime2);
+    }
+
+    private int[] getOverlapped(int start1, int end1, int start2, int end2) {
+        return new int[] { Math.max(start1, start2), Math.min(end1, end2)};
+    }
     
-//     public boolean book(int startTime, int endTime) {
-        
-//     }
-// }
+    public boolean book(int startTime, int endTime) {
+        for(int[] overBooking : overlapBookings) {//检查是否会三重预订
+            if(doesOverlap(startTime, endTime, overBooking[0], overBooking[1])){
+                return false;
+            }
+        }
+
+        for(int[] booking : bookings) {
+            if(doesOverlap( startTime,endTime,  booking[0],  booking[1])){
+                overlapBookings.add(getOverlapped(startTime, endTime, booking[0], booking[1]));
+            }
+        }
+        bookings.add(new int[]{startTime, endTime});
+        return true;
+    }
+}
 
 /**
  * Your MyCalendarTwo object will be instantiated and called as such:
@@ -15,30 +39,4 @@
  * boolean param_1 = obj.book(startTime,endTime);
  */
 
-class MyCalendarTwo {
-    TreeMap<Integer, Integer> map;
-
-    public MyCalendarTwo() {
-        map = new TreeMap<>((x, y) -> y - x);
-    }
-
-    public boolean book(int start, int end) {
-        int temp = 0;
-        int ans = 0;
-        map.put(start, map.getOrDefault(start, 0) + 1);
-        map.put(end, map.getOrDefault(end, 0) - 1);
-        //恢复所有的值 采取了逆向恢复 和正向恢复都一样
-        for (Integer key : map.keySet()) {
-            temp = temp - map.get(key);
-            ans = Math.max(ans, temp);
-            if (ans > 2) {
-                map.put(start, map.getOrDefault(start, 0) - 1);
-                map.put(end, map.getOrDefault(end, 0) + 1);
-                return false;
-            }
-        }
-        return true;
-    }
-
-}
 
