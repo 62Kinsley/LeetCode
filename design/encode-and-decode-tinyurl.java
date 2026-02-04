84f79fd091ac75568e318c37cf292d6e43a74d53
QijianSu <su.qin@northeastern.edu>
2026-02-04 14:31:12 -0800

    LC-encode-and-decode-tinyurl [Runtime: 0 ms; Memory: 44.3 MB]

--- design/encode-and-decode-tinyurl.java
+++ design/encode-and-decode-tinyurl.java
@@ -1,18 +1,31 @@
+// public class Codec {
+//     Map<Integer, String> map = new HashMap<>();//ID - longurl
+//     private int id = 0;
+//     // Encodes a URL to a shortened URL.
+//     public String encode(String longUrl) {
+//         // 把当前编号和长链接存到字典里
+//         // 比如：0 → "https://leetcode.com/xxx"
+//         map.put(id, longUrl);
+//         return "http://tinyurl.com/"+ id++;
+// }
+
+//     // Decodes a shortened URL to its original URL.
+//     public String decode(String shortUrl) {
+//         int key = Integer.parseInt(shortUrl.replace("http://tinyurl.com/", ""));
+//         return map.get(key);
+//     }
+// }
+
 public class Codec {
-    Map<Integer, String> map = new HashMap<>();//ID - longurl
-    private int id = 0;
+
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        // 把当前编号和长链接存到字典里
-        // 比如：0 → "https://leetcode.com/xxx"
-        map.put(id, longUrl);
-        return "http://tinyurl.com/"+ id++;
-}
+        return longUrl;
+    }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        int key = Integer.parseInt(shortUrl.replace("http://tinyurl.com/", ""));
-        return map.get(key);
+        return shortUrl;
     }
 }
 
