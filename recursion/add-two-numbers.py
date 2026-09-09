d4cbd3b6e67b6fc5994d31b08547c1a8531668c3
QijianSu <su.qin@northeastern.edu>
2026-09-08 21:08:05 -0700

    LC-add-two-numbers [Runtime: 4 ms; Memory: 19.3 MB]

--- recursion/add-two-numbers.py
+++ recursion/add-two-numbers.py
@@ -12,15 +12,16 @@ class Solution:
         cur = dummy
         carry = 0
   
-        while l1 or l2 or carry:
+        while l1 is not None or l2 is not None or carry != 0:
             
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
 
             sum = val1  + val2  + carry
+            digit = sum % 10
             carry = sum // 10
 
-            cur.next = ListNode(sum % 10)
+            cur.next = ListNode( digit)
             cur = cur.next
         
             if l1:
