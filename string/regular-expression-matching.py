93b1f1a871eca1854ff1f3cdc62fa2408aa94ab0
QijianSu <su.qin@northeastern.edu>
2026-09-10 20:01:18 -0700

    LC-regular-expression-matching [Runtime: 52 ms; Memory: 19.3 MB]

---
+++ string/regular-expression-matching.py
@@ -0,0 +1,29 @@
+#dp[i][j] means whether the first i characters of s match the first j characters of p.
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+
+        dp = [[False]*(n+1) for _ in range(m+1)]
+
+        dp[0][0] = True
+
+        for j in range(2, n+1):
+            if p[j-1] == '*' and dp[0][j-2]:
+                dp[0][j] = True
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == p[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    if s[i-1] == p[j-2] or p[j-2] == '.':
+                        dp[i][j] = dp[i][j-2] or dp[i][j-1] or dp[i-1][j]
+                    elif s[i-1] != p[j-2]:
+                        dp[i][j] = dp[i][j-2]    
+        
+        return dp[m][n]
+    
+
