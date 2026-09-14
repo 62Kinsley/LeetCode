7b816d33598df653458c27bbdd659a8d905c62b2
QijianSu <su.qin@northeastern.edu>
2026-09-13 23:04:26 -0700

    LC-swap-nodes-in-pairs [Runtime: 46 ms; Memory: 19.3 MB]

--- recursion/swap-nodes-in-pairs.py
+++ recursion/swap-nodes-in-pairs.py
@@ -3,25 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def swapPairs(self, head: OptionalListNode) -> OptionalListNode:
+#         dummy = ListNode()
+#         dummy.next = head
+#         prev = dummy
+
+#         while prev.next and prev.next.next:
+        
+#            first = prev.next
+#            second = prev.next.next
+
+#            first.next = second.next
+#            second.next = first
+#            prev.next = second
+           
+#            prev = first
+
+#         return dummy.next
+
+
 class Solution:
     def swapPairs(self, head: OptionalListNode) -> OptionalListNode:
-        dummy = ListNode()
-        dummy.next = head
-        prev = dummy
 
-        while prev.next and prev.next.next:
+        if not head or not head.next:
+            return head
         
-           first = prev.next
-           second = prev.next.next
+        node1 = head
+        node2 = head.next
+        node3 = head.next.next
 
-           first.next = second.next
-           second.next = first
-           prev.next = second
-           
-           prev = first
-
-        return dummy.next
-
-
-
+        node2.next = node1
+        node1.next = self.swapPairs(node3)
         
+        return node2
